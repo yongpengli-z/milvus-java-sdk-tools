5a4632700d642325f321886364a7b9f33f9c3f67
yongpengli-z <yongpeng.li@zilliz.com>
2024-09-19 10:07:45 +0800

    modify uri

--- src/main/java/custom/BaseTest.java
+++ src/main/java/custom/BaseTest.java
@@ -24,10 +24,10 @@ public class BaseTest {
   public static List<Object> recallBaseIdList=new ArrayList<>();
 
   public static void main(String[] args) {
-    String uri = "https://in01-d1b7fd43805d33d.aws-us-west-2.vectordb-uat3.zillizcloud.com:19531";
+    String uri = "https://in01-2fe732db8ac0299.aws-us-west-2.vectordb-uat3.zillizcloud.com:19531";
     String token = "6aff239bed5702130e09ad03a3379a71034a3e7b4160de384dce58c501e5bf98e49816c670b4b6335f51276c294976ce6ba25fa4";
     String initialParams = "{\"cleanCollection\":true}";
-    String customizeParams =  "{\"CreateCollectionParams_0\":{\"shardNum\":1,\"enableDynamic\":false,\"enableMmap\":false,\"fieldParamsList\":[{\"dataType\":\"Int64\",\"primaryKey\":true},{\"dataType\":\"FloatVector\",\"dim\":768,\"primaryKey\":false}],\"numPartitions\":0,\"collectionName\":\"\"},\"LoadParams_3\":{\"loadAll\":true},\"CreateIndexParams_2\":{\"collectionName\":\"\",\"indexParams\":[]},\"InsertParams_4\":{\"batchSize\":1000,\"collectionName\":\"\",\"numConcurrency\":1,\"numEntries\":1000000},\"SearchParams_5\":{\"collectionName\":\"\",\"filter\":\"\",\"nq\":5,\"numConcurrency\":50,\"outputs\":[],\"randomVector\":true,\"runningMinutes\":10,\"topK\":10,\"searchLevel\":1}}";
+    String customizeParams =  "{\"CreateCollectionParams_0\":{\"shardNum\":1,\"enableDynamic\":false,\"enableMmap\":false,\"fieldParamsList\":[{\"dataType\":\"Int64\",\"primaryKey\":true},{\"dataType\":\"FloatVector\",\"dim\":768,\"primaryKey\":false}],\"numPartitions\":0,\"collectionName\":\"\"},\"LoadParams_3\":{\"loadAll\":true},\"CreateIndexParams_2\":{\"collectionName\":\"\",\"indexParams\":[]},\"InsertParams_4\":{\"batchSize\":1000,\"collectionName\":\"\",\"numConcurrency\":1,\"numEntries\":100000},\"SearchParams_5\":{\"collectionName\":\"\",\"filter\":\"\",\"nq\":5,\"numConcurrency\":50,\"outputs\":[],\"randomVector\":true,\"runningMinutes\":10,\"topK\":10,\"searchLevel\":1}}";
     System.out.println(uri);
     System.out.println(token);
     System.out.println(initialParams);
