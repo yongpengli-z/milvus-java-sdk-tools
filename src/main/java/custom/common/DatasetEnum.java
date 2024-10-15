bfa4dcef742724dd0eca03ca8a5976ccc999aab8
yongpengli-z <yongpeng.li@zilliz.com>
2024-10-15 17:49:18 +0800

    add wait component6

--- src/main/java/custom/common/DatasetEnum.java
+++ src/main/java/custom/common/DatasetEnum.java
@@ -5,7 +5,7 @@ import io.milvus.v2.common.DataType;
 public enum DatasetEnum {
     GIST("gist","/mainctrfs/test/milvus/raw_data/gist1m/","binary_768d_",768),
     LAION("laion","./mainctrfs/test/milvus/raw_data/laion1b/","binary_768d_",768),
-    DEEP("deep",".../mainctrfs/test/milvus/raw_data/laion1b/","binary_96d__",96),
+    DEEP("deep",".../mainctrfs/test/milvus/raw_data/deep1b/","binary_96d__",96),
     SIFT("sift",".././mainctrfs/test/milvus/raw_data/sift1b/","binary_128d_",128);
 
     public final String datasetName;
