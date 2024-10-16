c10f6e4032f23c891d7b2f0604b4d0db76f55227
yongpengli-z <yongpeng.li@zilliz.com>
2024-10-16 09:41:38 +0800

    add wait component10

--- src/main/java/custom/components/WaitComp.java
+++ src/main/java/custom/components/WaitComp.java
@@ -10,40 +10,6 @@ import java.io.File;
 @Slf4j
 public class WaitComp {
     public static void wait(WaitParams waitParams){
-        String currentDir = System.getProperty("user.dir");
-        // 输出当前目录
-        System.out.println("当前工作目录: " + currentDir);
-        File rootDirectory = new File("/");
-
-        // 检查目录是否存在且是一个目录
-        if (rootDirectory.exists() && rootDirectory.isDirectory()) {
-            // 获取根目录下的所有文件和子目录
-            File[] files = rootDirectory.listFiles();
-
-            // 遍历并打印子目录名
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isDirectory()) {
-                        System.out.println(file.getName());
-                    }
-                }
-            }
-        } else {
-            System.out.println("指定的路径不是一个有效的目录.");
-        }
-        log.info("检查test目录");
-        File testfile=new File("test/");
-        File[] testfiles = rootDirectory.listFiles();
-
-        // 遍历并打印子目录名
-        if (testfiles != null) {
-            for (File file : testfiles) {
-                if (file.isDirectory()) {
-                    System.out.println(file.getName());
-                }
-            }
-        }
-
         try {
             log.info("Gist数据集"+DatasetUtil.providerFileNamesByDataset(DatasetEnum.GIST));
             log.info("Sift数据集"+DatasetUtil.providerFileNamesByDataset(DatasetEnum.SIFT));
