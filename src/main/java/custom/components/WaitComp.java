7df12698e3c18045c7ab029652ed6b2bd131c309
yongpengli-z <yongpeng.li@zilliz.com>
2024-10-16 17:06:40 +0800

    add wait component12

--- src/main/java/custom/components/WaitComp.java
+++ src/main/java/custom/components/WaitComp.java
@@ -6,15 +6,16 @@ import custom.utils.DatasetUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.util.List;
 
 @Slf4j
 public class WaitComp {
     public static void wait(WaitParams waitParams){
         try {
-            log.info("Gist数据集"+DatasetUtil.providerFileNamesByDataset(DatasetEnum.GIST));
-            log.info("Sift数据集"+DatasetUtil.providerFileNamesByDataset(DatasetEnum.SIFT));
-            log.info("Laion数据集"+DatasetUtil.providerFileNamesByDataset(DatasetEnum.LAION));
-            log.info("Deep数据集"+DatasetUtil.providerFileNamesByDataset(DatasetEnum.DEEP));
+            List<String> strings = DatasetUtil.providerFileNames(DatasetEnum.GIST);
+            List<Float> floats = DatasetUtil.providerFloatVectorByDataset(199999, strings, DatasetEnum.GIST);
+            System.out.println(floats);
+            System.out.println(floats.size());
 
             log.info("Waiting "+ waitParams.getWaitMinutes()+" minutes...");
             Thread.sleep(waitParams.getWaitMinutes()*60*1000);
--- src/main/java/custom/utils/DatasetUtil.java
+++ src/main/java/custom/utils/DatasetUtil.java
@@ -2,27 +2,26 @@ package custom.utils;
 
 import custom.common.DatasetEnum;
 import lombok.extern.slf4j.Slf4j;
+import org.openlca.npy.Npy;
+import org.openlca.npy.arrays.NpyArray;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 @Slf4j
 public class DatasetUtil {
 
-    public static List<String> providerFileNames(String filePrefixName,String path){
+    public static List<String> providerFileNames(DatasetEnum datasetEnum) {
         List<String> fileNameList = new ArrayList<>();
-        File file = new File(path);
+        File file = new File(datasetEnum.path);
         // 检查目录是否存在且是一个目录
         if (file.exists() && file.isDirectory()) {
             // 获取目录下的所有文件和子目录
             File[] files = file.listFiles();
-
             // 遍历并打印包含指定关键字的文件名
             if (files != null) {
                 for (File fileItem : files) {
-                    if (fileItem.isFile() && fileItem.getName().contains(filePrefixName)) {
+                    if (fileItem.isFile() && fileItem.getName().contains(datasetEnum.prefixName)) {
                         fileNameList.add(fileItem.getName());
                     }
                 }
@@ -31,27 +30,58 @@ public class DatasetUtil {
             log.error("指定的路径不是一个有效的目录.");
         }
         // 排序
-        fileNameList.sort(Comparator.comparingInt(x->
-                        Integer.parseInt(x.substring(x.lastIndexOf("_")+1,x.lastIndexOf("."))))
-                );
+        fileNameList.sort(Comparator.comparingInt(x ->
+                Integer.parseInt(x.substring(x.lastIndexOf("_") + 1, x.lastIndexOf("."))))
+        );
         return fileNameList;
     }
 
-    public static List<String> providerFileNamesByDataset(DatasetEnum datasetEnum){
 
-        return providerFileNames(datasetEnum.prefixName, datasetEnum.path);
+    public static List<Float> providerFloatVectorByDataset(long index, List<String> fileNames, DatasetEnum datasetEnum) {
+        long countIndex=0;
+        long countIndexTemp=countIndex;
+        List<Float> floatList=new ArrayList<>();
+        Iterator<String> iterator = fileNames.iterator();
+        while(iterator.hasNext()){
+            String npyDataPath = datasetEnum.path+iterator.next();
+            File file = new File(npyDataPath);
+            NpyArray<?> npyArray = Npy.read(file);
+            countIndex+=npyArray.shape()[0];
+            System.out.println(countIndex);
+            if(countIndex > index) {
+                System.out.println("查到数据");
+                float[] floatData = (float[]) npyArray.data();
+                List<List<Float>> floats = splitArray(floatData, npyArray.shape()[1]);
+                floatList=floats.get((int) (index-countIndexTemp));
+                break;
+            }
+            countIndexTemp=countIndex;
+            iterator.remove();
+//            System.out.println(iterator.next());
+        }
+        return floatList;
     }
 
+    public static List<List<Float>> splitArray(float[] array, int chunkSize) {
+        List<List<Float>> chunks = new ArrayList<>();
+        List<Float> chunk;
 
-    public static List<Float> providerFloatVectorByDataset(long index,String filePrefixName,String path) {
+        for (int i = 0; i < array.length; i += chunkSize) {
+            chunk = new ArrayList<>();
+            for (int j = i; j < i + chunkSize && j < array.length; j++) {
+                chunk.add(array[j]);
+            }
+            chunks.add(chunk);
+        }
 
-        return null;
+        return chunks;
     }
 
     public static void main(String[] args) {
-
-        List<String> strings = providerFileNamesByDataset(DatasetEnum.GIST);
-        System.out.println(strings);
+        List<String> strings = providerFileNames(DatasetEnum.GIST);
+        List<Float> floats = providerFloatVectorByDataset(199999, strings, DatasetEnum.GIST);
+        System.out.println(floats);
+        System.out.println(floats.size());
 
     }
 }
