6d736524ef9c372ef6d155df30b398b34831d52a
qixuan <673771573@qq.com>
2024-12-05 14:51:38 +0800

    add searchIterator for v1 and v2
    Signed-off-by: qixuan <673771573@qq.com>

---
+++ src/main/java/custom/entity/SearchIteratorParams.java
@@ -0,0 +1,23 @@
+package custom.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SearchIteratorParams {
+    private String collectionName;
+    private int nq;
+    private int topK;
+    private boolean randomVector;
+    private String vectorFieldName;
+    private List<String> outputs;
+    private String filter;
+    private String metricType;
+    private int numConcurrency;
+    private long runningMinutes;
+    private String params;
+    private String indexAlgo;
+    private int batchSize;
+    private boolean useV1;
+}
