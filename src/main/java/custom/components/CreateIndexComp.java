fd413e46c5e56ec9ab4a7b9b2b5e3402b57bc1a2
yongpengli-z <yongpeng.li@zilliz.com>
2025-01-15 17:27:19 +0800

    add describe index

--- src/main/java/custom/components/CreateIndexComp.java
+++ src/main/java/custom/components/CreateIndexComp.java
@@ -5,14 +5,19 @@ import custom.entity.CreateIndexParams;
 import custom.entity.result.CommonResult;
 import custom.entity.result.CreateIndexResult;
 import custom.entity.result.ResultEnum;
+import io.milvus.v2.service.index.request.DescribeIndexReq;
+import io.milvus.v2.service.index.response.DescribeIndexResp;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 import static custom.BaseTest.globalCollectionNames;
+import static custom.BaseTest.milvusClientV2;
 
 @Slf4j
 public class CreateIndexComp {
     public static CreateIndexResult CreateIndex(CreateIndexParams createIndexParams) {
-        String collectionName = (createIndexParams.getCollectionName() == null || createIndexParams.getCollectionName().equals("")) ? globalCollectionNames.get(globalCollectionNames.size()-1) : createIndexParams.getCollectionName();
+        String collectionName = (createIndexParams.getCollectionName() == null || createIndexParams.getCollectionName().equals("")) ? globalCollectionNames.get(globalCollectionNames.size() - 1) : createIndexParams.getCollectionName();
         CommonResult commonResult;
         try {
             CommonFunction.createCommonIndex(collectionName, createIndexParams.getIndexParams());
@@ -22,6 +27,14 @@ public class CreateIndexComp {
             commonResult = CommonResult.builder().result(ResultEnum.EXCEPTION.result)
                     .message(e.getMessage()).build();
         }
+        // describe index
+        DescribeIndexResp describeIndexResp = milvusClientV2.describeIndex(DescribeIndexReq.builder().collectionName(collectionName).build());
+        List<DescribeIndexResp.IndexDesc> indexDescriptions =
+                describeIndexResp.getIndexDescriptions();
+        for (DescribeIndexResp.IndexDesc indexDescription : indexDescriptions) {
+            log.info(indexDescription.getFieldName() + "-" + indexDescription.getIndexType() + "-" + indexDescription.getIndexState());
+        }
+
         return CreateIndexResult.builder()
                 .collectionName(collectionName)
                 .indexParams(createIndexParams.getIndexParams())
