dd47b9b127bd57f76e52137cb33a8d51f6bd9b57
yongpengli-z <yongpeng.li@zilliz.com>
2025-03-28 17:05:47 +0800

    support UpdateIndexPoolComp2

--- src/main/java/custom/components/UpdateIndexPoolComp.java
+++ src/main/java/custom/components/UpdateIndexPoolComp.java
@@ -1,6 +1,7 @@
 package custom.components;
 
 import com.alibaba.fastjson.JSONObject;
+import custom.common.ComponentSchedule;
 import custom.entity.UpdateIndexPoolParams;
 import custom.entity.result.CommonResult;
 import custom.entity.result.ResultEnum;
@@ -8,6 +9,8 @@ import custom.entity.result.UpdateIndexPoolResult;
 import custom.pojo.IndexPoolInfo;
 import custom.utils.CloudOpsServiceUtils;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @Slf4j
 public class UpdateIndexPoolComp {
@@ -19,7 +22,16 @@ public class UpdateIndexPoolComp {
         }
         if(!(updateIndexPoolParams.getWorkerImageTag()==null ||
                 updateIndexPoolParams.getWorkerImageTag().equalsIgnoreCase(""))){
-            indexPoolInfo.setWorkerImageTag(updateIndexPoolParams.getWorkerImageTag());
+            // image重新获取
+            String latestImageByKeywords;
+            if(updateIndexPoolParams.getWorkerImageTag().equalsIgnoreCase("latest-release")){
+                List<String> strings = ComponentSchedule.queryReleaseImage();
+                latestImageByKeywords = strings.get(0);
+                indexPoolInfo.setManagerImageTag(latestImageByKeywords.substring(latestImageByKeywords.indexOf("(")+1,latestImageByKeywords.indexOf(")")));
+            }else{
+                latestImageByKeywords = CloudOpsServiceUtils.getLatestImageByKeywords(updateIndexPoolParams.getWorkerImageTag());
+                indexPoolInfo.setManagerImageTag(latestImageByKeywords.substring(latestImageByKeywords.indexOf("(")+1,latestImageByKeywords.indexOf(")")));
+            }
         }
         String s = CloudOpsServiceUtils.updateIndexPool(indexPoolInfo);
         JSONObject jsonObject = JSONObject.parseObject(s);
