5b81b75b5e9e4383eb98c1a9d710b42692986a3a
yongpengli-z <yongpeng.li@zilliz.com>
2025-10-22 10:36:54 +0800

    add result log

--- src/main/java/custom/components/SearchComp.java
+++ src/main/java/custom/components/SearchComp.java
@@ -144,7 +144,7 @@ public class SearchComp {
                             SearchResp search = milvusClientV2.search(searchReq);
                             long endItemTime = System.currentTimeMillis();
                             float costTimeItem = (float) ((endItemTime - startItemTime) / 1000.00);
-                            log.info("线程[" + finalC + "]  search cost:" + costTimeItem + " s");
+                            log.info("线程[" + finalC + "]  search cost:" + costTimeItem + " s"+"，result size："+search.getSearchResults().size());
                             costTime.add(costTimeItem);
                             returnNum.add(search.getSearchResults().get(0).size());
                             if (printLog >= logInterval) {
