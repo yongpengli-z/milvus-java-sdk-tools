13124cdd9badbfcdb7e48ce92a4b69fb7e59f804
yongpengli-z <yongpeng.li@zilliz.com>
2026-09-22 15:44:06 +0800

    modify backup dataset

--- src/main/java/custom/common/BackupDatasetEnum.java
+++ src/main/java/custom/common/BackupDatasetEnum.java
@@ -3,7 +3,7 @@ package custom.common;
 import custom.config.EnvEnum;
 
 public enum BackupDatasetEnum {
-    AWS_LAION_768D_2_5M("aws-laion-768d-2.5m", "laion_768d_2.5m", EnvEnum.AWS_WEST, "laion", 768, 2_500_000L, "backup11_6a5e6e322049eb5", "in01-d97eed4bad83877"),
+    AWS_LAION_768D_2_5M("aws-laion-768d-2.5m", "laion_768d_2.5m", EnvEnum.AWS_WEST, "laion", 768, 2_500_000L, "backup11_515bde91174ee17", "in01-596c6d787e35006"),
     AWS_LAION_768D_8M("aws-laion-768d-8m", "laion_768d_8m", EnvEnum.AWS_WEST, "laion", 768, 8_000_000L, "backup11_9eb2bf7571e5638", "in01-6ac3a6811b1d9f1"),
     AWS_LAION_768D_40M("aws-laion-768d-40m", "laion_768d_40m", EnvEnum.AWS_WEST, "laion", 768, 40_000_000L, "backup11_9668da2c3f00b3a", "in01-1e65192c6585f6e");
 
